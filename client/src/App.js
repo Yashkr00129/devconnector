99b43ad29fe2cc8a5acb812efcd556ee34f973d0
Yash Sharma <yashkr00129@gmail.com>
2021-11-17 16:08:06 +0530

    Starting Redux

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Navbar } from "./Components/Layout/Navbar.jsx";
+import { Landing } from "./Components/Layout/Landing.jsx";
+import { Register } from "./Components/Auth/Register.jsx";
+import { Login } from "./Components/Auth/Login.jsx";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Landing />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -134,4 +134,59 @@ router.put("/unlike/:id", auth, async (req, res) => {
   }
 });
 
+// @route  POST api/posts/comment/:id
+// @desc   Comment on a post
+// access  Private
+router.post(
+  "/comment/:id",
+  [auth, [check("text", "Text is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+      const post = await Post.findById(req.params.id);
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        user: req.user.id,
+        avatar: user.avatar,
+      };
+      post.comments.unshift(newComment);
+      await post.save();
+      res.json(post.comments);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+// @route  DELETE api/posts/comment/:id/:comment_id
+// @desc   Delete a comment
+// access  Private
+router.delete("/comment/:id/:comment_id", auth, async (req, res) => {
+  try {
+    // Get Comment
+    const post = await Post.findById(req.params.id);
+    const comments = post.comments;
+    // Check if it is users comment
+    const comment = comments.find((c) => c.id == req.params.comment_id);
+    if (!comment) return res.status(404).json({ msg: "No Comment Like this" });
+    if (comment.user.toString() !== req.user.id)
+      return res.status(401).json({ msg: "Not authorised" });
+    // Delete Comment
+    const index = comments.findIndex((c) => c.id == req.params.comment_id);
+    comments.splice(index, 1);
+    post.save();
+    res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
