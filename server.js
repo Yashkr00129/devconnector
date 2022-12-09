9d504549592daad96b48185da9fee82b01f1352f
yash sharma <yashkr00129@gmail.com>
2022-12-09 11:56:18 +0530

    Include hosting code.

--- server.js
+++ server.js
@@ -16,14 +16,14 @@ app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/github", require("./routes/api/github"));
 
-// //Server Static assets in production
-// if(process.env.NODE_ENV==='production'){
-// //  Set static folder
-//   app.use(express.static("client/build"))
-//   app.get('*',(req,res)=>{
-//     res.sendFile(path.resolve(__dirname,"client","build","index.html"))
-//   })
-// }
+//Server Static assets in production
+if(process.env.NODE_ENV==='production'){
+//  Set static folder
+  app.use(express.static("client/build"))
+  app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"client","build","index.html"))
+  })
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () =>
